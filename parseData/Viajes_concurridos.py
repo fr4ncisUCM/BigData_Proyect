c77bd45471ba7d3f24375d07013182361f5b7894
fr4ncisUCM <87377072+fr4ncisUCM@users.noreply.github.com>
2022-12-01 15:27:15 +0100

    mayor dest , ini y total

--- parseData/Viajes_concurridos.py
+++ parseData/Viajes_concurridos.py
@@ -1,32 +1,47 @@
 from pyspark.sql.functions import udf
 import sys
+from pyspark.sql.functions import col, substring
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import concat, lit, col, asc
 
 spark_app = SparkSession.builder.appName('ViajesCount').getOrCreate()
 
 df = spark_app.read.format("csv").option("header", "true").load(sys.argv[1])
+dfIni = spark_app.read.format("csv").option("header", "true").load(sys.argv[1])
+dfFin = spark_app.read.format("csv").option("header", "true").load(sys.argv[1])
 
 # How many trips do we want
 # number_of_trips = sys.argv[1]
 
 # Valid columns
 validC = ['startingAirport', 'destinationAirport']
-
+validCIni = ['startingAirport']
+validCFin = ['destinationAirport']
 # fit the dataframe
 df = df[validC]
+dfIni = dfIni[validCIni]
+dfFin = dfFin[validCFin]
 
 
-def join_airports(dataframe):
-    a = str(dataframe.destinationAirport)
-    b = str(dataframe.startingAirport)
-    if a < b:
+def join_airports(ini, fin):
+    # AC -- KN
+    # KN -- AC
+    if ini < fin:
         return "startingAirport", "destinationAirport"
     else:
         return "destinationAirport", "startingAirport"
 
 
-df1 = df.select("*", concat(col(join_airports(df)[0]), lit(" "), col(join_airports(df)[1])).alias("trip")) \
+df1 = df.select("*", concat(substring('startingAirport', 0, 3), lit(" "), substring('destinationAirport', 0, 3)).alias("trip")) \
     .groupBy("trip").count() \
     .orderBy("count", ascending=False)
 df1.show()
+
+
+df2 = dfIni.groupBy('startingAirport').count()\
+        .orderBy("count", ascending=False)
+df2.show()
+
+df3 = dfFin.groupBy('destinationAirport').count()\
+        .orderBy("count", ascending=False)
+df3.show()
