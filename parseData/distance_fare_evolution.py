38e485ddde2c4d0ed2498e35375aebcbfe6e50fa
fr4ncisUCM <87377072+fr4ncisUCM@users.noreply.github.com>
2022-12-07 16:25:18 +0100

    distace_fare evolution PD - Fix the most common airlines ( shows the most common airports)

---
+++ parseData/distance_fare_evolution.py
@@ -0,0 +1,29 @@
+import sys
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import substring
+from pyspark.sql.functions import concat, lit, col, asc
+
+spark_app = SparkSession.builder.appName('empresas').getOrCreate()
+
+df = spark_app.read.format("csv").option("header", "true").load(sys.argv[1])
+
+# Valid columns
+validC = ['startingAirport', 'destinationAirport', 'totalTravelDistance', 'totalFare']
+
+# fit the dataframe
+df = df[validC]
+
+df1 = df.dropna()\
+    .select("*", concat(substring('startingAirport', 0, 3), lit(" "), substring('destinationAirport', 0, 3)).alias("trip")) \
+    .groupBy('trip').avg('totalFare', 'totalTravelDistance')\
+    .drop(['startingAirport', 'destinationAirport'])
+df1.show()
+
+# Convert to a pandas Dataframe
+pandas_df = df1.toPandas()
+
+# Create a line plot
+fig = pandas_df.plot().figure
+
+# Save the plot to a file
+fig.savefig("evolution.png")
