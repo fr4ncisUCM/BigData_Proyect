a344e27ef6e9bd2da9571c28ee9fff350d4d1fce
dgallee <100680484+dgallee@users.noreply.github.com>
2022-12-07 16:56:06 +0100

    Update VuelosB.py

--- parseData/VuelosB.py
+++ parseData/VuelosB.py
@@ -4,15 +4,17 @@ import sys
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import concat, lit, col, asc, split
 
-spark_app = SparkSession.builder.appName('precio_Compañia').getOrCreate()
+spark_app = SparkSession.builder.appName('DateFly').getOrCreate()
 
 df = spark_app.read.format("csv").option("header", "true").load(sys.argv[1])
 
-validC = ['startingAirport', 'destinationAirport', 'totalFare', 'segmentsAirlineName']
+validC = ['startingAirport', 'destinationAirport', 'totalFare', 'segmentsAirlineCode']
 
 df = df[validC]
 
-df1 = df.withColumn('CodeCompany', split(df['segmentsAirlineCode'], '\\|\\|').getItem(0).drop('segmentsAirlineCode')
+df.printSchema()
+
+df1 = df.withColumn('CodeCompany', split(df['segmentsAirlineCode'], '\\|\\|').getItem(0).drop('segmentsAirlineCode'))
 
 df2 = df1.withColumn("totalFare", col("totalFare").cast('float'))
 
