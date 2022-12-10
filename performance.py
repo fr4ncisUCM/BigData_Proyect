1feb3ceeec3e432a1a4c4d82323a0802f93b4097
fr4ncisUCM <87377072+fr4ncisUCM@users.noreply.github.com>
2022-12-10 20:34:24 +0100

    performance

---
+++ performance.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Create a dictionary with the data for the DataFrame
+data = {'TripsPerDay.py': [26, 23, 22],
+        'EmpresasFind.py': [62, 58, 57],
+        'Viajes_concurridos.py': [77, 76, 73]}
+
+# Create the DataFrame using the dictionary and specify the index and column names
+df = pd.DataFrame(data, index=['2-Nod', '3-Nod', '5-Nod'])
+
+# Use the DataFrame.plot() method to create a bar plot
+df.plot(kind='bar', title='My Bar Plot')
+
+# Save the plot to a file
+plt.show()
